f4be76998898d7ce1d11389cd7a286521a2d487f
Pranjal Goyal <pranjalgoyal13@gmail.com>
2024-12-13 12:00:05 +0530

    Archival

--- groww_chatbot/chatbot/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import FAQ, Category, CategoryMap
-# Register your models here.
-admin.site.register(FAQ)
-admin.site.register(Category)
-admin.site.register(CategoryMap)
